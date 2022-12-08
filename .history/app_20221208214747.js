b8f5da7bed7bfaa198287ebc03bce2cbf9dd24f5
Andrew <andrew.huynh561@gmail.com>
2022-12-08 22:18:14 +1030

    completed at 41:06p

---
+++ .history/app_20221208214747.js
@@ -0,0 +1,19 @@
+var search = document.querySelector('.search')
+var city = document.querySelector('.city')
+var country = document.querySelector('.country')
+var value = document.querySelector('.value')
+var shortDesc = document.querySelector('.short-desc')
+var visibility = document.querySelector('.visibility span')
+var sun = document.querySelector('.sun span')
+var wind = document.querySelector('.wind span')
+
+
+async function changeWeatherUI(){
+    search.value.trim()
+    let apiURL ='https://api.openweathermap.org/data/2.5/weather?lat=Tokyo&lon=metric&appid=a1e51a2e1930131165494fcf7790daad'
+
+    let data = await fetch(apiURL).then(res=> res.json())
+    console.log(data);
+}
+
+changeWeatherUI()
